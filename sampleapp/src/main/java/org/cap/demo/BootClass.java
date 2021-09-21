0953b52364b6378b50806a11c596048b01fd1415
sravani1922k <sravani.sribhashyam@gmail.com>
2021-09-21 12:03:08 +0530

    array added in show method

--- sampleapp/src/main/java/org/cap/demo/BootClass.java
+++ sampleapp/src/main/java/org/cap/demo/BootClass.java
@@ -8,6 +8,11 @@ public class BootClass {
 
 	}
 	public static void show() {
+		int[] arr= {12,34,56,56};
+		for(int i=0;i<100;i++)
+		{
+			System.out.println(arr[i]);
+		}
 		System.out.println("show method here");
 		}
 
